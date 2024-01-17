5258b5a960df8d8caf74f2a0f0b9bc985d19368b
Juan H.M Rafael <juan0412@live.com>
2024-01-17 00:13:48 -0400

    sin cambios

--- inmuebles/urls.py
+++ inmuebles/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import InmueblesListView, inmueble_individual, get_paises, get_estados, get_municipios
+from .views import  InmueblesListView, inmueble_individual, get_paises, get_estados, get_municipios
 
 app_name = 'inmuebles'
 
 urlpatterns = [
     path('', InmueblesListView.as_view(), name='home'),
+    
     path('en-paises/', get_paises, name='get_paises'),
 
     path('en-paises/<str:paises_seleccionados>',
